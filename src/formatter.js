bf2e5bf8c3d3ed32262fe8183ae9838de66c8ec4
Bowser65 <bowoser@weeb.services>
2020-06-04 15:13:52 +0200

    New welcome strings (& change license)

--- src/formatter.js
+++ src/formatter.js
@@ -1,19 +1,6 @@
 /*
- * Small microservice to generate a Discord-like chat section
- * Copyright (C) 2020 Bowser65
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (c) 2020 Bowser65
+ * Licensed under the Open Software License version 3.0
  */
 
 const fit = require('./commons/fit')
@@ -334,45 +321,19 @@ module.exports = class Formatter {
 
   _computeWelcomeMessage (msg) {
     const messages = [
-      '<@%user%> just joined the server - glhf!',
-      '<@%user%> just joined. Everyone, look busy!',
-      '<@%user%> just joined. Can I get a heal?',
-      '<@%user%> joined your party.',
-      '<@%user%> joined. You must construct additional pylons.',
-      'Ermagherd. <@%user%> is here.',
-      'Welcome, <@%user%>. Stay awhile and listen.',
-      'Welcome, <@%user%>. We were expecting you ( ͡° ͜ʖ ͡°)',
+      '<@%user%> joined the party.',
+      '<@%user%> is here.',
       'Welcome, <@%user%>. We hope you brought pizza.',
-      'Welcome <@%user%>. Leave your weapons by the door.',
       'A wild <@%user%> appeared.',
-      'Swoooosh. <@%user%> just landed.',
-      'Brace yourselves. <@%user%> just joined the server.',
-      '<@%user%> just joined... or did they?',
-      '<@%user%> just arrived. Seems OP - please nerf.',
+      '<@%user%> just landed.',
       '<@%user%> just slid into the server.',
-      'A <@%user%> has spawned in the server.',
-      'Big <@%user%> showed up!',
-      'Where’s <@%user%>? In the server!',
-      '<@%user%> hopped into the server. Kangaroo!!',
-      '<@%user%> just showed up. Hold my beer.',
-      'Challenger approaching - <@%user%> has appeared!',
-      'It\'s a bird! It\'s a plane! Nevermind, it\'s just <@%user%>.',
-      'It\'s <@%user%>! Praise the sun! \\\\[T]/',
-      'Never gonna give <@%user%> up. Never gonna let <@%user%> down.',
-      '<@%user%> has joined the battle bus.',
-      'Cheers, love! <@%user%>\'s here!',
-      'Hey! Listen! <@%user%> has joined!',
-      'We\'ve been expecting you <@%user%>',
-      'It\'s dangerous to go alone, take <@%user%>!',
-      '<@%user%> has joined the server! It\'s super effective!',
-      'Cheers, love! <@%user%> is here!',
-      '<@%user%> is here, as the prophecy foretold.',
-      '<@%user%> has arrived. Party\'s over.',
-      'Ready player <@%user%>',
-      '<@%user%> is here to kick butt and chew bubblegum. And <@%user%> is all out of gum.',
-      'Hello. Is it <@%user%> you\'re looking for?',
-      '<@%user%> has joined. Stay a while and listen!',
-      'Roses are red, violets are blue, <@%user%> joined this server with you'
+      '<@%user%> just showed up!',
+      'Welcome <@%user%>. Say hi!',
+      '<@%user%> hopped into the server.',
+      'Everyone welcome <@%user%>!',
+      'Glad you\'re here, <@%user%>.',
+      'Good to see you, <@%user%>.',
+      'Yay you made it, <@%user%>!'
     ]
 
     // eslint-disable-next-line no-undef
